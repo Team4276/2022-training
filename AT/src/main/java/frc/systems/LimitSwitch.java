be555dd846682014c5f70178738579c21d249b50
JolyA <jsabdelmesseh100@student.hbuhsd.edu>
2021-12-15 21:34:35 -0800

    Added LimitSwitch.java class to local repository

---
+++ AT/src/main/java/frc/systems/LimitSwitch.java
@@ -0,0 +1,28 @@
+
+//Import  packages and files requiered for the class
+package frc.systems;
+import frc.robot.Robot;
+import edu.wpi.first.hal.simulation.DIODataJNI;
+
+
+public class LimitSwitch extends DIODataJNI {//Class declaration: "public class 'Name of the class' {}"
+    
+    //create instance variables that will be used throughout the code (See Unit 1 on APCS videos in slack)
+    
+    boolean LSwitchStatus; //stores a True/False value on whether or not the limit switch is activated
+
+    //constructor: determine the characteristics of the limit switch
+    public LimitSwitch(int limitSwitchPort, boolean LSwitch){
+        LSwitchStatus = LSwitch;
+        
+    }//end constructor
+    
+    
+    public boolean isArmTouching()
+    {   
+        return Robot.myLimitSwitch.LSwitchStatus;
+    
+    }//end method
+
+
+}//end class
